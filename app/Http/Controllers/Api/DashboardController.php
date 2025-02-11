22200f95df9d5d12e207de25071b14d7d7c4dd73
MohaEttaibi <mohammed.ettayby@gmail.com>
2025-02-11 20:38:58 +0100

    Fetching brands & products

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Brand;
+use App\Models\Product;
 
 class DashboardController extends Controller
 {
     public function home() {
+        $brand =  Brand::all();
+        $product =  Product::all();
         return response()->json([
-            'status'=>true,
-            'message'=>'App Home Page',
-            'data'=>[]
+            'status' => true,
+            'message' => 'App Home Page',
+            'brand' => $brand,
+            'product' => $product,
         ], 200);
 
     }

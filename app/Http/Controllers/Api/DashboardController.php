56d90fd2ce2b7aa9661396fe20521e55537a20d7
MohaEttaibi <mohammed.ettayby@gmail.com>
2025-02-12 19:06:57 +0100

    Get Products By Brand

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -11,13 +11,26 @@ class DashboardController extends Controller
 {
     public function home() {
         $brand =  Brand::all();
-        $product =  Product::all();
+        $product =  Product::where('available', '=', 1)->get();
         return response()->json([
             'status' => true,
-            'message' => 'App Home Page',
+            'message' => 'Home Page',
             'brand' => $brand,
             'product' => $product,
         ], 200);
+    }
 
+    public function products_by_brand($brand) {
+        $brands = Brand::where('id', '=', $brand)->first();
+        $product = Product::where([
+            ['available', '=', 1],
+            ['brand', '=', $brands->id]
+        ])->get();
+        return response()->json([
+            'status' => true,
+            'message' => 'Products By Brand Page',
+            'brand' => $brand,
+            'products' => $product,
+        ], 200);
     }
 }

21fbb6b2dc01cd8682c46f500ea07b8dc53951b0
MohaEttaibi <mohammed.ettayby@gmail.com>
2025-02-15 19:11:50 +0100

    Register

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DashboardController;
+use App\Http\Controllers\Api\AuthCotroller;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,4 +14,8 @@ Route::controller(DashboardController::class)->group(function() {
     Route::get('products-by-brand/{brand}', 'products_by_brand');
     Route::get('products-view/{id}', 'products_view');
     Route::get('filters/{filter}', 'filters');
+});
+
+Route::controller(AuthCotroller::class)->group(function () {
+    Route::post('register', 'register');
 });
